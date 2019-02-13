a1109e3e4bd8ff7a4b7fa7fd34080ecbbc606280
Rainiugnas <aqdtvz9@gmail.com>
2019-02-13 18:45:06 +0100

    token-auth - add token authentication to routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticated_user!
+
   layout 'success'
 
   def render_error status, data
     @data = data || {}
 
     render 'layouts/error', layout: false, status: status
+  end
+
+  def authenticated_user!
+    token = Token.new request.headers[:Authorization]
+
+    render_error :unauthorized, message: 'invalid token' if token.invalid?
   end
 end
--- app/services/token.rb
+++ app/services/token.rb
@@ -14,6 +14,7 @@ class Token
   def valid?
     @valid ||= @data.present? && @data['expiration'].to_time >= Time.now
   end
+  def invalid?() valid? == false end
 
   def to_jwt() @jwt end
 
