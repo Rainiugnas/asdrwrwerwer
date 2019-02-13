a40ad6514f277fe5a3a207dc3f795135d6a82911
Rainiugnas <aqdtvz9@gmail.com>
2019-02-13 14:36:01 +0100

    json-layer - use success as default layout and add method to simplify error layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  layout 'success'
+
+  def render_error status, data
+    @data = data || {}
+
+    render 'layouts/error', layout: false, status: status
+  end
 end
